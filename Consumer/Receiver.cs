ec76a93c5bc2b44d314e091f1c6f6a6891b86eaf
Melany Odette Eusebio Beltre <melany.eusebio@solvex.com.do>
2023-03-14 18:06:16 -0400

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,28 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "xmelan",
+    Password = "Orange07"
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("XmelanQueue", false, false, false, null);
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Mensaje recibido {0}", message);
+    };
+
+    channel.BasicConsume("XmelanQueue", true, consumer);
+    Console.WriteLine("Presiona [enter] para salir del consumer");
+    Console.ReadLine();
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,23 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "xmelan",
+    Password = "Orange07",
+};
+
+using ( var connection = factory.CreateConnection())
+using ( var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("XmelanQueue", false, false, false, null);
+    var message = "Bienvenido al curso de RabbitMQ y .NET";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "XmelanQueue", null, body);
+    Console.WriteLine("El mensaje fue enviado {0}", message);
+}
+
+Console.WriteLine("Presiona [enter] para salir de la aplicacion");
+Console.ReadLine();
